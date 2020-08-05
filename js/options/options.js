e7e000c9607840d454273c51db18a0bbaecb2088
Roger Raventos <rogerito03@live.com>
2020-08-05 18:23:35 +0200

    inici de les opcions + imgs

---
+++ js/options/options.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    getMessage("general", "general-tab")
+});
+
+function getMessage(msg, item) {
+    var message = chrome.i18n.getMessage(msg);
+    document.getElementById(item).innerHTML = message;
+}
