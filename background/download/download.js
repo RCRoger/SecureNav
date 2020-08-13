0607e22b1c4b769dbedea769b64855625af48033
Roger Raventos <rogerito03@live.com>
2020-08-13 19:51:52 +0200

    save

--- background/download/download.js
+++ background/download/download.js
@@ -1,82 +1,83 @@
-var urls = undefined;
-var max_size = undefined;
-var type = undefined;
-var enabled = undefined;
+function DownloadBackground() {
+    this.urls = undefined;
+    this.max_size = undefined;
+    this.type = undefined;
+    this.enabled = undefined;
+    this.add_dwl_blocker_listener();
+}
 
-function url_block(file){
-    if (Array.isArray(urls) && urls.length){
-        return urls.some(function(item){
-            if(item.exec(file.url) || item.exec(file.finalUrl))
-                return true;
+(function (DB, undefined) {
+    DB.prototype.url_block = function (file) {
+        if (Array.isArray(this.urls) && this.urls.length) {
+            return urls.some(function (item) {
+                if (item.exec(file.url) || item.exec(file.finalUrl))
+                    return true;
+            });
+        }
+        return false;
+    }
+
+    DB.prototype.size_block = function (file) {
+        return this.max_size > file.fileSize;
+    }
+
+    DB.prototype.download_blocker = function (file) {
+        chrome.downloads.pause(file.id);
+        console.log('Pausada');
+
+        if (this.url_block(file) || this.size_block(file)) {
+            chrome.downloads.cancel(file.id);
+            console.log('Cancelada');
+            //TODO add pop up info
+        }
+        else {
+            chrome.downloads.resume(file.id);
+            console.log('resumida');
+        }
+    }
+
+    DB.prototype.download_allower = function (file) {
+        chrome.downloads.pause(file.id);
+        console.log('Pausada');
+
+        if (url_block(file) || size_block(file)) {
+            chrome.downloads.resume(file.id);
+            console.log('resumida');
+
+            //TODO add pop up info
+        }
+        else {
+            chrome.downloads.cancel(file.id);
+            console.log('Cancelada');
+        }
+    }
+
+    DB.prototype.add_dwl_blocker_listener = function () {
+        var that = this;
+        this.urls = [];
+        chrome.storage.local.get(['dwl_enabled', 'dwl_type', 'dwl_url_list', 'dwl_max_size'], function (data) {
+            chrome.downloads.onCreated.removeListener(dwl_listener);
+            that.enabled = data.dwl_enabled;
+            if (!that.enabled) {
+                return;
+            }
+            that.type = data.dwl_type;
+            data.dwl_url_list.forEach(item => that.urls.push(url_regex(item)));
+            that.max_size = data.dwl_max_size;
+            if ((Array.isArray(that.urls) && that.urls.length) || that.max_size) {
+                chrome.downloads.onCreated.addListener(dwl_listener);
+            }
         });
     }
-    return false;   
-}
+})(DownloadBackground);
 
-function size_block(file){
-    return max_size > file.fileSize;
-}
+var dwl = new DownloadBackground();
 
-function dwl_listener(file){
-    if(type == 0){
-        download_allower(file);
+var dwl_listener = function (file) {
+    if (dwl.type == 0) {
+        dwl.download_allower(file);
     }
-    else if(type == 1){
-        download_blocker(file);
+    else if (dwl.type == 1) {
+        dwl.download_blocker(file);
     }
 }
-
-function download_blocker(file){
-    chrome.downloads.pause(file.id);
-    console.log('Pausada');
-
-    if (url_block(file) || size_block(file)){
-        chrome.downloads.cancel(file.id);
-        console.log('Cancelada');
-        //TODO add pop up info
-    }
-    else{
-        chrome.downloads.resume(file.id);
-        console.log('resumida');
-    }
-}
-
-function download_allower(file){
-    chrome.downloads.pause(file.id);
-    console.log('Pausada');
-    
-    if (url_block(file) || size_block(file)){
-        chrome.downloads.resume(file.id);
-        console.log('resumida');
-        
-        //TODO add pop up info
-    }
-    else{
-        chrome.downloads.cancel(file.id);
-        console.log('Cancelada');
-    }
-}
-
-function add_dwl_blocker_listener(){
-    urls = [];
-    chrome.storage.local.get(['dwl_enabled', 'dwl_type', 'dwl_url_list', 'dwl_max_size'], function(data){
-        chrome.downloads.onCreated.removeListener(dwl_listener);
-        if(!data.dwl_enabled){
-            return;
-        }
-        type = data.dwl_type;
-        data.dwl_url_list.forEach(item => urls.push(url_regex(item)));
-        max_size = data.dwl_max_size;
-        if ((Array.isArray(urls) && urls.length) || max_size) {
-            chrome.downloads.onCreated.addListener(dwl_listener);
-        }
-    });
-}
-
-
-chrome.extension.onRequest.addListener(function(request) {
-    if (request && (request.id == "update_dwl"))
-        add_dwl_blocker_listener();
-  });
-
-add_dwl_blocker_listener();
